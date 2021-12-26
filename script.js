b8119a28b25a6d9d0700805046c9871b85f3fa93
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-12-26 21:18:41 +0300

    Challenge: Convert given seconds to space age on all planets of our solar system

--- script.js
+++ script.js
@@ -1,51 +1,33 @@
-let numberArr = [" zero"," one"," two"," three"," four"," five"," six"," seven"," eight"," nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
-let tensArr = [" twenty"," thirty"," forty"," fifty"," sixty"," seventy"," eighty"," ninety"];
+const spaceAge = (seconds) => {
+  // Earth years, 365.25 Earth days or:
+  const EARTHYEARSSECONDS = 31557600;
+  const yearEarth = seconds / EARTHYEARSSECONDS;
+  const yearMultiplier = {
+      Mercury: 0.2408467,
+      Venus: 0.61519726,
+      Earth: 1,
+      Mars: 1.8808158,
+      Jupiter: 11.862615,
+      Saturn: 29.447498,
+      Uranus: 84.016846,
+      Neptune: 164.79132,
+  };
 
-function sayNumber(number) {
-    let sayBaby = "";
-    let tens = +String(Math.floor(number/10)).slice(-1);
-    let hundred = Math.floor(number/100);
-    console.log({number})
-    if(hundred){
-      console.log(hundred)
-      sayBaby += numberArr[hundred] + " hundred";
-  } 
-    
-     if(tens <2) {
-        number = String(number);
-        number = +number.slice(number.length-2);
-        sayBaby += numberArr[number] 
-    } else {
-        sayBaby += tensArr[tens-2];
-        number = String(number);
-        number = +number.slice(number.length-1);
-        if(number){
-        sayBaby += numberArr[number];
+const yearsInAllPlanets = {
+  Mercury: 0,
+  Venus: 0,
+  Earth: 0,
+  Mars: 0,
+  Jupiter: 0,
+  Saturn: 0,
+  Uranus: 0,
+  Neptune: 0,
+}
+  Object.keys(yearsInAllPlanets).forEach((planet) => {
+      yearsInAllPlanets[planet] = (yearEarth / yearMultiplier[planet]).toFixed(2); 
+  })
 
-        }
-    }
-    return sayBaby;
+return yearsInAllPlanets
 }
 
-const sayNumberInEnglish = (n /* ADD MORE PARAMETERS IF NEEDED */) => {
-	let sayDude = "";
-    let numberBillion = +String(Math.floor(n/10**9)).slice(-3);
-    let numberMillion = +String(Math.floor(n/10**6)).slice(-3)
-    let numberThousand = +String(Math.floor(n/10**3)).slice(-3);
-    if(numberBillion) {
-        sayDude += sayNumber(numberBillion) + " billion";
-    }
-    if(numberMillion) {
-        sayDude += sayNumber(numberMillion) + " million";
-    }
-    if(numberThousand) {
-        sayDude += sayNumber(numberThousand) + " thousand";
-    }
-    
-    let neo = Number(String(n).substr(-3,3));
-     sayDude += sayNumber(neo);
-    
-	return sayDude
-}
-
-console.log(`12_127_325 in english is: ${sayNumberInEnglish(2802761)}`)
+console.log(spaceAge(Math.round(81281621)))
