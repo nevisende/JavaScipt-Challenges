2d00be3540e930adad9cc3c58b3a8f33f47d43b0
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-12-26 17:09:12 +0300

    Challenge: Given a number from 0 to 999,999,999,999, spell out that number in English.

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-function fibonacci(n) {
-  return n < 1 ? 0
-  : n <= 2 ? 1
-  : fibonacci(n - 1) + fibonacci(n - 2)}
-console.log( fibonacci(10))
+let numberArr = [" zero"," one"," two"," three"," four"," five"," six"," seven"," eight"," nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"];
+let tensArr = [" twenty"," thirty"," forty"," fifty"," sixty"," seventy"," eighty"," ninety"];
+
+function sayNumber(number) {
+    let sayBaby = "";
+    let tens = +String(Math.floor(number/10)).slice(-1);
+    let hundred = Math.floor(number/100);
+    console.log({number})
+    if(hundred){
+      console.log(hundred)
+      sayBaby += numberArr[hundred] + " hundred";
+  } 
+    
+     if(tens <2) {
+        number = String(number);
+        number = +number.slice(number.length-2);
+        sayBaby += numberArr[number] 
+    } else {
+        sayBaby += tensArr[tens-2];
+        number = String(number);
+        number = +number.slice(number.length-1);
+        if(number){
+        sayBaby += numberArr[number];
+
+        }
+    }
+    return sayBaby;
+}
+
+const sayNumberInEnglish = (n /* ADD MORE PARAMETERS IF NEEDED */) => {
+	let sayDude = "";
+    let numberBillion = +String(Math.floor(n/10**9)).slice(-3);
+    let numberMillion = +String(Math.floor(n/10**6)).slice(-3)
+    let numberThousand = +String(Math.floor(n/10**3)).slice(-3);
+    if(numberBillion) {
+        sayDude += sayNumber(numberBillion) + " billion";
+    }
+    if(numberMillion) {
+        sayDude += sayNumber(numberMillion) + " million";
+    }
+    if(numberThousand) {
+        sayDude += sayNumber(numberThousand) + " thousand";
+    }
+    
+    let neo = Number(String(n).substr(-3,3));
+     sayDude += sayNumber(neo);
+    
+	return sayDude
+}
+
+console.log(`12_127_325 in english is: ${sayNumberInEnglish(2802761)}`)
