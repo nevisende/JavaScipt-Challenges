50ff122dda91f9f43692248cc5ea10e18be5bc50
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-12-28 23:40:44 +0300

    Challenge: Regexp Basics - is it a vowel?

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-const spaceAge = (seconds) => {
-  // Earth years, 365.25 Earth days or:
-  const EARTHYEARSSECONDS = 31557600;
-  const yearEarth = seconds / EARTHYEARSSECONDS;
-  const yearMultiplier = {
-      Mercury: 0.2408467,
-      Venus: 0.61519726,
-      Earth: 1,
-      Mars: 1.8808158,
-      Jupiter: 11.862615,
-      Saturn: 29.447498,
-      Uranus: 84.016846,
-      Neptune: 164.79132,
-  };
+String.prototype.vowel = function() {
+  return /^([aeiou])+$/i.test(this);
+};
 
-const yearsInAllPlanets = {
-  Mercury: 0,
-  Venus: 0,
-  Earth: 0,
-  Mars: 0,
-  Jupiter: 0,
-  Saturn: 0,
-  Uranus: 0,
-  Neptune: 0,
-}
-  Object.keys(yearsInAllPlanets).forEach((planet) => {
-      yearsInAllPlanets[planet] = (yearEarth / yearMultiplier[planet]).toFixed(2); 
-  })
+console.log("eioua".vowel())
+// true
 
-return yearsInAllPlanets
-}
-
-console.log(spaceAge(Math.round(81281621)))
+console.log("eika".vowel())
+// false
