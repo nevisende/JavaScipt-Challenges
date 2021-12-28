83331eadd25e0f491526655e379ea36a303b5710
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-12-29 00:01:00 +0300

    Challenge: Anchorize me!

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-String.prototype.vowel = function() {
-  return /^([aeiou])+$/i.test(this);
-};
+function anchorize(text){
+  const result = text.split(' ').map(x=> {
+    if(/[http:|https:|ftp:|ftps:|file:|smb:]/.test(x)) {
+      return `<a href="${x}">${x}</a>`
+    } 
+    return x
+  })
+  return result.join(' ')
+  }
 
-console.log("eioua".vowel())
-// true
+  console.log('<xmp>'+anchorize('hello http://world.com !')+'</xmp>')
+  // '<xmp>hello <a href="http://world.com">http://world.com</a> !</xmp>'
 
-console.log("eika".vowel())
-// false
+  console.log('<xmp>'+anchorize('Hello FTP://World.com')+'</xmp>')
+  // '<xmp>Hello <a href="FTP://World.com">FTP://World.com</a></xmp>'
